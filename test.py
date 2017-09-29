4d190a3da7c5622fe63da65084fa577dcb1ebc06
Firas Chaaben <firaschaabencss@gmail.com>
2017-09-29 20:36:31 +0100

    fix image diff result

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ from PIL import ImageChops
 
 im1 = Image.open("img1.png")
 im2 = Image.open("img2.png")
-diff = ImageChops.difference(im2,im1) 
+diff = ImageChops.difference(im1,im1) 
 print(type(diff))
-if diff is None:
+if not diff.getbbox():
     print("equal")
 diff.show()
 print(diff)
