894e4b29849ec61d5ba8a59ee772c9313cadf682
Firas Chaaben <firaschaabencss@gmail.com>
2017-09-30 18:57:58 +0100

    fix display youtube top 10

--- top10_youtube.py
+++ top10_youtube.py
@@ -24,8 +24,13 @@ for top in top10[:10]:
 """
 
 top10 = browser.find_elements(By.XPATH, "//a[contains(@class, 'yt-simple-endpoint style-scope ytd-video-renderer')]")
+print("\n \n")
+i = 1
 for top in top10[:10]:
     about = top.get_attribute("aria-label")
+    print("top ",i)
     print(about)
+    print("\n \n")
+    i = i + 1
     
 browser.close()
