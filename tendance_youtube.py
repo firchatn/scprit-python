990264dd85269d549a3480805b3938dd15c51cda
Firas Chaaben <firaschaabencss@gmail.com>
2017-09-30 16:45:48 +0100

    fix value probleam

--- tendance_youtube.py
+++ tendance_youtube.py
@@ -10,7 +10,18 @@ time.sleep(10)
 
 top = browser.find_element_by_xpath("//a[contains(@class, 'yt-simple-endpoint style-scope ytd-video-renderer')]")
 last_top = top	.get_attribute("title")
-print(last_top)
+
+list = browser.find_elements(By.XPATH, "//span[contains(@class, 'style-scope ytd-video-meta-block')]")
+last_vue list[0].text
+last_date_title list[1].text
+
+#vue = browser.find_element_by_xpath("//span[contains(@class, 'style-scope ytd-video-meta-block')]")
+#last_vue = vue.text
+
+print('title : ',last_top)
+print('nombre vue : ',last_vue)
+print('poster le : ',last_date_title)
+
 browser.close()
 
 
