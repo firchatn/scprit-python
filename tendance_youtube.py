8e28a0f9915baa66ad9ad367fb68a48a67489246
Firas Chaaben <firaschaabencss@gmail.com>
2017-09-30 17:16:09 +0100

    top 10 youtube

--- tendance_youtube.py
+++ tendance_youtube.py
@@ -9,7 +9,7 @@ browser.get('http://youtube.com/feed/trending')
 time.sleep(10)
 
 top = browser.find_element_by_xpath("//a[contains(@class, 'yt-simple-endpoint style-scope ytd-video-renderer')]")
-last_top = top	.get_attribute("title")
+last_top = top.get_attribute("title")
 
 list = browser.find_elements(By.XPATH, "//span[contains(@class, 'style-scope ytd-video-meta-block')]")
 last_vue = list[0].text
@@ -23,5 +23,3 @@ print('nombre vue : ',last_vue)
 print('poster le : ',last_date_title)
 
 browser.close()
-
-
---
+++ top10_youtube.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+browser = webdriver.Firefox()
+browser.get('http://youtube.com/feed/trending')
+
+time.sleep(10)
+"""
+top10 = browser.find_elements(By.XPATH, "//a[contains(@class, 'yt-simple-endpoint style-scope ytd-video-renderer')]")
+for top in top10[:10]:
+
+    last_top = top.get_attribute("title")
+
+    list = browser.find_elements(By.XPATH, "//span[contains(@class, 'style-scope ytd-video-meta-block')]")
+    last_vue = list[0].text
+    last_date_title = list[1].text
+
+
+    print('title : ',last_top)
+    print('nombre vue : ',last_vue)
+    print('poster le : ',last_date_title)
+"""
+
+top10 = browser.find_elements(By.XPATH, "//a[contains(@class, 'yt-simple-endpoint style-scope ytd-video-renderer')]")
+for top in top10[:10]:
+    about = top.get_attribute("aria-label")
+    print(about)
+    
+browser.close()
