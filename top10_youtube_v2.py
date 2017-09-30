83ed945bb36f56a5a7fbf0a9a2fb9a81ac726047
Firas Chaaben <firaschaabencss@gmail.com>
2017-09-30 22:30:56 +0100

    to fix phontomjs

--- top10_youtube_v2.py
+++ top10_youtube_v2.py
@@ -6,11 +6,14 @@ import time
 
 browser = webdriver.PhantomJS(service_args=['--ssl-protocol=any'])
 browser.set_window_size(1120, 550)
-#browser.maximize_window()
+browser.maximize_window()
 try:
+	
 	browser.get("https://youtube.com/feed/trending/")
 	browser.maximize_window()
-
+	print('ok0')
+	top10 = browser.find_elements(By.XPATH, "//a[contains(@class, 'yt-simple-endpoint style-scope ytd-video-renderer')]")
+	print(top10)
 	print('ok')
 	top = browser.find_element_by_id('video-title')
 	print('ok2')
