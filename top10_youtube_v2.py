b3ba2a5bdb15bae974a8c64ebc45d4711dd22448
Firas Chaaben <firaschaabencss@gmail.com>
2017-09-30 20:33:46 +0100

    try support server version

---
+++ top10_youtube_v2.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+
+browser = webdriver.PhantomJS(service_args=['--ssl-protocol=any'])
+browser.set_window_size(1120, 550)
+#browser.maximize_window()
+try:
+	browser.get("https://youtube.com/feed/trending/")
+	browser.maximize_window()
+
+	print('ok')
+	top = browser.find_element_by_id('video-title')
+	print('ok2')
+	about = top.get_attribute("aria-label")
+	print('ok3')
+	print(about)
+except Exception :
+	browser.save_screenshot('screenshot.png')
+browser.close()
