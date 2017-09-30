16717de3f43efe5f2886a4c435d4549b0719205c
Firas Chaaben <firaschaabencss@gmail.com>
2017-09-30 10:54:07 +0100

    support 40 follow

--- insta.py
+++ insta.py
@@ -5,7 +5,9 @@ import time
 
 browser = webdriver.Firefox()
 browser.get('http://instagram.com')
+
 time.sleep(10)
+
 phone_email = browser.find_element_by_name('emailOrPhone')
 phone_email.clear()
 phone_email.send_keys('mohsenijkel@gmail.com')
@@ -21,15 +23,18 @@ user_name.send_keys('mohseniju8547')
 password_el = browser.find_element_by_name('password')
 password_el.clear()
 password_el.send_keys('azertycom')
+
 password_el.send_keys(Keys.RETURN)
 
-time.sleep(10)
-browser.find_element_by_xpath("//a[contains(@class, '_8scx2 _gvoze coreSpriteDesktopNavExplore')]").click()
-time.sleep(5)
-browser.find_element_by_xpath("//a[contains(@class, '_3f3gc _fkers')]").click()
-time.sleep(10)
-list = browser.find_elements(By.XPATH, "//button[contains(@class, '_qv64e _gexxb _4tgw8 _njrw0')]")
-for i in list:
-    i.click()
-    print(i.text)
+for j in range(4):
+    time.sleep(10)
+    browser.find_element_by_xpath("//a[contains(@class, '_8scx2 _gvoze coreSpriteDesktopNavExplore')]").click()
+    time.sleep(5)
+    browser.find_element_by_xpath("//a[contains(@class, '_3f3gc _fkers')]").click()
+    time.sleep(10)
+    list = browser.find_elements(By.XPATH, "//button[contains(@class, '_qv64e _gexxb _4tgw8 _njrw0')]")
+    for i in range(5):
+        time.sleep(1)
+        list[i].click()
+        print(list[i].text)
 
