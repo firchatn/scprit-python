94146a91431af7b2bb782cd040fd1efc1540d220
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-01 12:00:25 +0100

    fix all phantomJS probleam && stable youtube server version

--- top10_youtube_v2.py
+++ top10_youtube_v2.py
@@ -6,22 +6,28 @@ import time
 
 browser = webdriver.PhantomJS("/home/firas/git/scripts-python/phantomjs-2.1.1-linux-x86_64/bin/phantomjs",  service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'])
 browser.set_window_size(1120, 550)
-try:
-	print(browser.current_url)
-	browser.get("https://youtube.com/feed/trending/")
-	time.sleep(10)
-	browser.maximize_window()
-	print(browser.current_url)
-	print('ok0')
 
-	top10 = browser.find_elements(By.XPATH, "//a[contains(@class, 'yt-simple-endpoint style-scope ytd-video-renderer')]")
-	print(top10)
-	print('ok')
-	top = browser.find_element_by_id('video-title')
-	print('ok2')
-	about = top.get_attribute("aria-label")
-	print('ok3')
-	print(about)
-except Exception :
-	browser.save_screenshot('screenshot.png')
+print(browser.current_url)
+browser.get("https://youtube.com/feed/trending/")
+time.sleep(10)
+browser.maximize_window()
 
+top10 = browser.find_elements(By.XPATH, "//a[contains(@class, 'yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink      spf-link ')]")
+top10info = browser.find_elements(By.XPATH, "//ul[contains(@class, 'yt-lockup-meta-info')]")
+
+"""
+time.sleep(5)
+for i in info:
+	texte = i.text
+	print(texte)
+"""
+time.sleep(5)
+k = 0 
+for top in top10:
+	info = top10info[k].find_elements_by_tag_name("li")
+	t = top.get_attribute("title")
+	print("title = ",t)
+	for i in info:
+		texte = i.text
+		print(texte)
+	k = k + 1
