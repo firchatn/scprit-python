9f8c2054d386d510ee4b6e129c01e2140c014295
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-03 21:43:24 +0100

    new follow script

---
+++ insta_follow.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import random, string
+
+
+
+
+while True:
+    def randomword(length):
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for i in range(length))
+
+
+    email_rand = randomword(10) + '@gmail.com'
+    user_rand = randomword(5) + '.' + randomword(5) 
+
+    print(email_rand)
+    print(user_rand)
+
+    browser = webdriver.Firefox()
+    browser.get('http://instagram.com')
+
+    time.sleep(10)
+
+    phone_email = browser.find_element_by_name('emailOrPhone')
+    phone_email.clear()
+    phone_email.send_keys(email_rand)
+
+    full_name = browser.find_element_by_name('fullName')
+    full_name.clear()
+    full_name.send_keys('jake guidci')
+
+    user_name = browser.find_element_by_name('username')
+    user_name.clear()
+    user_name.send_keys(user_rand)
+
+    password_el = browser.find_element_by_name('password')
+    password_el.clear()
+    password_el.send_keys('azertycom')
+
+    password_el.send_keys(Keys.RETURN)
+    time.sleep(5)
+    browser.get('http://instagram.com/ala.hmida/')
+    time.sleep(5)
+    browser.find_element_by_xpath("//button[contains(@class, '_qv64e _gexxb _r9b8f _njrw0')]").click()
+    browser.close()
+
+
