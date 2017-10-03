090b21acc897e8ec0fc53206610dbf4fdcb60f25
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-03 21:22:47 +0100

    new script

--- insta_plus.py
+++ insta_plus.py
@@ -3,30 +3,35 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-browser = webdriver.Firefox()
+#browser = webdriver.Firefox()
+
+
+browser = webdriver.PhantomJS("/home/firas/git/scripts-python/phantomjs-2.1.1-linux-x86_64/bin/phantomjs",  service_args=['--ignore-ssl-errors=true', '--ssl-protocol=any'])
+browser.set_window_size(1120, 550)
+
+
 browser.get('http://instagram.com')
 
 time.sleep(10)
 
-phone_email = browser.find_element_by_name('emailOrPhone')
-phone_email.clear()
-phone_email.send_keys('mohsenijkel@gmail.com')
+browser.find_element_by_xpath("//a[contains(@class, '_b93kq')]").click()
 
-full_name = browser.find_element_by_name('fullName')
-full_name.clear()
-full_name.send_keys('mohsenrt')
 
 user_name = browser.find_element_by_name('username')
 user_name.clear()
-user_name.send_keys('mohseniju8547')
+user_name.send_keys('alalala')
 
 password_el = browser.find_element_by_name('password')
 password_el.clear()
-password_el.send_keys('azertycom')
+password_el.send_keys('testtttt')
 
 password_el.send_keys(Keys.RETURN)
 
-for j in range(8):
+time.sleep(10)
+error = browser.find_element_by_id('slfErrorAlert')
+print(error.text)
+"""
+while True:
     time.sleep(10)
     browser.find_element_by_xpath("//a[contains(@class, '_8scx2 _gvoze coreSpriteDesktopNavExplore')]").click()
     time.sleep(5)
@@ -38,7 +43,6 @@ for j in range(8):
         if list[i].text == 'Follow':
             list[i].click()
         print(list[i].text)
+"""
 
-
-browser.find_element_by_xpath("//a[contains(@class, '_8scx2 _gvoze coreSpriteDesktopNavProfile')]").click()
 
