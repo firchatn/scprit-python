e87517b30b3d882227cb0bb72195df9e98c0ae75
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-05 11:26:27 +0100

    create 3 folder

--- tst.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-driver = webdriver.PhantomJS("/home/firas/git/scripts-python/phantomjs-2.1.1-linux-x86_64/bin/phantomjs", service_args=['--ssl-protocol=any'])
-driver.set_window_size(1120, 550)
-driver.get("https://duckduckgo.com/")
-driver.find_element_by_id('search_form_input_homepage').send_keys("realpython")
-driver.find_element_by_id("search_button_homepage").click()
-print(driver.current_url)
-driver.quit()
-
