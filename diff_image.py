c03d5071e7ffbac128ae65af52e7cd8f45747432
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-07 17:18:00 +0100

    format code diff image python

--- diff_image.py
+++ diff_image.py
@@ -3,11 +3,9 @@ from PIL import ImageChops
 
 im1 = Image.open("img1.png")
 im2 = Image.open("img2.png")
-diff = ImageChops.difference(im1,im1) 
-print(type(diff))
+diff = ImageChops.difference(im1,im2) 
 if not diff.getbbox():
     print("equal")
 else:
     print("different see the diff")
 diff.show()
-print(diff)
