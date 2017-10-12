99373c483a7d8240c37ffe25cd0343119d13624c
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-12 16:00:01 +0100

    reformule folder struct

--- diff_image.py
+++ diff_image.py
@@ -1,8 +1,8 @@
 from PIL import Image
 from PIL import ImageChops
 
-im1 = Image.open("img1.png")
-im2 = Image.open("img2.png")
+im1 = Image.open("images/img1.png")
+im2 = Image.open("images/img2.png")
 diff = ImageChops.difference(im1,im2) 
 if not diff.getbbox():
     print("equal")
