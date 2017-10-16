6ae635ade70d34858ee76ce42412860e9b84e3a0
Firas Chaaben <firaschaabencss@gmail.com>
2017-10-16 11:32:55 +0100

    update todo list

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
+# open server in one script
+# port 4443
 
 httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
